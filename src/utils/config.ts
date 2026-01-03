56c0914b122561f71b857210882c4c34d52cf237
Asad Ali <asadalithaheem92@gmail.com>
2026-01-04 00:34:23 +0500

    set path of frontend url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3006'
-    : 'https://accounts.muxstoreweb.store';
+    : 'https://frontend.muxcloudweb.store';
 
 export default API_BASE_URL;
